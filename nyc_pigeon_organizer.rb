b5ed9beb8c664088a8dc31c368338f2e87765c30
kallaugher <alicekallaugher@gmail.com>
2016-06-23 13:44:57 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  pigeon_list = Hash.new { |hash, key| hash[key] = Hash.new(&hash.default_proc) }
+
+  data.each do |attribute, attribute_data|
+
+    attribute_data.each do |feature, names|
+
+      names.each do |name|
+
+        if pigeon_list[name].has_key?(attribute) == false
+          pigeon_list[name][attribute] = []
+          pigeon_list[name][attribute] << feature.to_s
+        else
+          pigeon_list[name][attribute] << feature.to_s
+        end
+
+      end
+
+    end
+
+  end
+
+  return pigeon_list
+
 end
